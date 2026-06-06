8d1091531662ad53dc444ac1801c112c74d20c30
togrul880 <59150360+togrul880@users.noreply.github.com>
2026-06-06 18:12:50 +0400

    added all files

---
+++ invProject/invApp/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import Product
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = "__all__"
+        labels = {
+            "product_id": "Product Id",
+            "name": "Name",
+            "sku": "SKU",
+            "price": "Price",
+            "quantity": "Quantity",
+            "supplier": "Supplier",
+        }
+        widgets = {
+            "product_id": forms.NumberInput(
+                attrs={"placeholder": "e.g. 1", "class": "form-control"}
+            ),
+            "name": forms.TextInput(
+                attrs={"placeholder": "e.g. shirt", "class": "form-control"}
+            ),
+            "sku": forms.TextInput(
+                attrs={"placeholder": "e.g. s12345", "class": "form-control"}
+            ),
+            "price": forms.NumberInput(
+                attrs={"placeholder": "e.g. 19.99", "class": "form-control"}
+            ),
+            "quantity": forms.NumberInput(
+                attrs={"placeholder": "e.g. 10", "class": "form-control"}
+            ),
+            "supplier": forms.TextInput(
+                attrs={"placeholder": "e.g. ABC Corp", "class": "form-control"}
+            ),
+        }
